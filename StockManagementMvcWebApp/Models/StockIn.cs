245bfbb1f2d204cd92c2fcd924b5c9459cbf9b98
Abiduddin <abidakash456@gmail.com>
2018-11-18 05:17:41 +0600

    Update-0.5
    validation error solved

--- StockManagementMvcWebApp/Models/StockIn.cs
+++ StockManagementMvcWebApp/Models/StockIn.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,14 @@ namespace StockManagementMvcWebApp.Models
 {
     public class StockIn
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "please select a company")]
         public int CompanyId { get; set; }
+        [Required(ErrorMessage = "please select an item")]
         public int ItemId { get; set; }
         public int ReorderLevel { get; set; }
         public double Available { get; set; }
+        [Required(ErrorMessage = "Please enter stockin quantit number")]
         public double StockInQuantity { get; set; }
     }
 }

20fc1aac275ee7d9a8d544bfe320315d61e79bf8
Abiduddin <abidakash456@gmail.com>
2018-11-29 07:41:09 +0600

    update-0.8
    stockIn
    stockOut

--- StockManagementMvcWebApp/Manager/StockManager.cs
+++ StockManagementMvcWebApp/Manager/StockManager.cs
@@ -13,6 +13,7 @@ namespace StockManagementMvcWebApp.Manager
         StockGateway aStockGateway = new StockGateway();
         public string SaveStockIn(StockIn stockIn)
         {
+            stockIn.Date = DateTime.Now.ToString("yyyy-MM-dd");
 
             var rowAffected = aStockGateway.SaveStockIn(stockIn);
             if (rowAffected[0] > 0 && rowAffected[1] > 0)
@@ -35,6 +36,26 @@ namespace StockManagementMvcWebApp.Manager
         {
             List<StockSummary> stockSummaries = aStockGateway.GetStockInformation();
             return stockSummaries;
+        }
+
+        public string StockOut_SaveAll(List<StockOut> stockOutList)
+        {
+            foreach (var stock in stockOutList)
+            {
+                stock.Date = DateTime.Now.ToString("yyyy-MM-dd");
+            }
+
+            int rowAffected = aStockGateway.StockOut_SaveAll(stockOutList);
+
+            if (rowAffected > 0)
+            {
+                return "Saved";
+            }
+            else
+            {
+                return "Failed";
+            }
+
         }
     }
 }
---
+++ StockManagementMvcWebApp/Scripts/stockInAjax.js
@@ -0,0 +1,112 @@
+﻿
+$(document).ready(function() {
+
+    $("#CompanyId").change(function () {
+        $("#reorderLevelInputBox").val(null);
+        $("#availabilityInputBox").val(null);
+
+        var companyId = $("#CompanyId").val();
+        var json = { companyId: companyId };
+        $("#ItemId").empty();
+        $.ajax({
+            type: "POST",
+            url: "/Stock/GetItemsByCompanyId",
+            contentType: "application/json; charset = utf-8",
+            data: JSON.stringify(json),
+            success: function (data) {
+                
+                $("#ItemId").append(
+                        "<option value=0>--Select Item--</option>"
+                    );
+                /*console.log(data);*/
+                $.each(data, function(key, value) {
+                    $("#ItemId").append(
+                        "<option value="+value.Id +
+                        ">"+ value.Name+"</option>"
+                    );
+                });
+
+            }
+    });
+
+    });
+
+
+    
+
+    $("#ItemId").change(function () {
+        var itemId = $("#ItemId").val();
+
+        console.log(itemId);
+        var json = { itemId: itemId };
+
+        $.ajax({
+            type: "POST",
+            url: "/Stock/GetItemsById",
+            contentType: "application/json; charset = utf-8",
+            data: JSON.stringify(json),
+            success: function (data) {
+
+                $("#reorderLevelInputBox").val(data[0].ReorderLevel);
+                $("#availabilityInputBox").val(data[0].Available);
+            }
+        });
+
+    });
+
+
+
+
+
+    
+
+    $("#saveStockIn").click(function () {
+        /*        console.log("ajax - 1");*/
+
+        var CompanyId = $("#CompanyId").val();
+        var ItemId = $("#ItemId").val();
+        var ReorderLevel = $("#reorderLevelInputBox").val();
+        var Available = $("#availabilityInputBox").val();
+        var StockInQuantity = $("#StockInQuantity").val();
+        console.log(ReorderLevel);
+        console.log(StockInQuantity);
+
+
+        var json = {
+            CompanyId: CompanyId,
+            ItemId: ItemId,
+            ReorderLevel: ReorderLevel,
+            Available: Available,
+            StockInQuantity: StockInQuantity
+        };
+
+        $.validator.unobtrusive.parse($("#stockForm"));
+        $("#stockForm").validate();
+        if ($("#stockForm").valid()) {
+            $.ajax({
+                type: "POST",
+                url: "/Stock/StockIn_Save",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(json),
+                success: function (data) {
+                    /*console.log("ajax - 2");*/
+ 
+
+                    $("#message").text(data);
+
+                    $("#CompanyId").val(null);
+                    $("#ItemId").val(null);
+                    $("#reorderLevelInputBox").val(null);
+                    $("#availabilityInputBox").val(null);
+                    $("#StockInQuantity").val(null);
+
+                }
+            });
+        } else {
+            $("#message").text("");
+        }
+
+    });
+
+
+});
